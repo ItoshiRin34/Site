975de27c48cba9326d4eaf0b5a88fe20b07113f3
ItoshiRin34 <botmeup666@gmail.com>
2026-03-02 13:58:43 -0500

    Inicio do site

---
+++ Back-End/arrasa/src/main/java/_Mosqueteiros/arrasa/ArrasaApplication.java
@@ -0,0 +1,13 @@
+package _Mosqueteiros.arrasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrasaApplication.class, args);
+	}
+
+}
---
+++ Front-End/script.js
@@ -0,0 +1,4 @@
+function avisar() {
+            alert("O primeiro passo de um grande desenvolvedor! Segue o jogo! SOCORRO ESTOU EM GRANDE SOFRIMENTO");
+            
+        } 
